1f6aec1e68891b6a068eff77e09ea3677cc6bd40
PiaSommerauer <pia.sommerauer@live.com>
2023-12-18 12:57:16 +0100

    set wikipedia to target language

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import utils_wikipedia
 
 
 target_qs = ['Q50729731']
-languages = ['en', 'nl', 'it', 'es', 'de', 'fr']
+# en is complete
+#languages = ['nl', 'it', 'es', 'de', 'fr']
+languages = ["it"]
 
-# number of articles you try to crawl (set to 20 for testing)
+# number of aicles you try to crawl (set to 20 for testing)
 cutoff = -1 # set to -1 to get all references
 
 for target_q in target_qs:
